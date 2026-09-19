7d888da571a5dbbfbd2286f5f4621152bd3c72e5
real_shafique <shafiqurrahmanansari0786@gmail.com>
2026-09-19 14:16:37 +0530

    Connect frontend to deployed API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -55,7 +55,7 @@ function App() {
       setLoading(true);
 
       const response = await fetch(
-        "http://127.0.0.1:8000/predict",
+        "https://last-min-forecasting-1.onrender.com/predict",
         {
           method: "POST",
 
